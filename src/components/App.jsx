0eb8f3e02ac3c2d857c688b557909c597affca15
Michael Haas <haas@computerlinguist.org>
2020-04-30 13:47:45 +0200

    Introduce ZoneSelector component + jest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
+// @flow
 import React from 'react'
 
-import Plot from './Plot'
+// import Plot from './Plot'
 
 import { hot } from 'react-hot-loader/root'
 
-import * as zones from '../data/zone'
+// import * as zones from '../data/zone'
 
-class App extends React.Component {
+import ZoneSelector from './ZoneSelector.jsx'
+
+class App extends React.Component<{}> {
   render () {
-    return <Plot polygons={[zones.ScaeCore, zones.RaoApproximate, zones.LeebRogallaApproximate]} />
+    const zoneSelection = {
+      'scae-core': {
+        label: 'SCAE Core',
+        checked: false
+      },
+      scaa: {
+        label: 'SCAA',
+        checked: true
+      }
+    }
+    return <ZoneSelector zoneSelection={zoneSelection} />
+    // return <Plot polygons={[zones.ScaeCore, zones.RaoApproximate, zones.LeebRogallaApproximate]} />
   }
 }
 export default hot(App)
---
+++ src/components/ZoneSelector.test.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ZoneSelector from './ZoneSelector'
+import renderer from 'react-test-renderer'
+
+it('renders correctly', () => {
+  const zoneSelection = {
+    'scae-core': {
+      label: 'SCAE Core',
+      checked: false
+    },
+    scaa: {
+      label: 'SCAA',
+      checked: true
+    }
+  }
+
+  const tree = renderer
+    .create(<ZoneSelector zoneSelection={zoneSelection} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders correctly empty zoneSelection correctly', () => {
+  const zoneSelection = {}
+  const tree = renderer
+    .create(<ZoneSelector zoneSelection={zoneSelection} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('onChange callback works', () => {
+  const zoneSelection = {
+    'scae-core': {
+      label: 'SCAE Core',
+      checked: false
+    },
+    scaa: {
+      label: 'SCAA',
+      checked: true
+    }
+  }
+
+  const callback = jest.fn()
+
+  const tree = renderer
+    .create(<ZoneSelector value={zoneSelection} onChange={callback}/>)
+
+})
+
+
+
